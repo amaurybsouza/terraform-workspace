4e7be6eebbc04155a676f6f32374d6a344ed469e
amaurybsouza <amaurybsouza@gmail.com>
2020-04-28 17:36:55 -0300

    new resources

--- aws-ec2-terraform1.0/instance.tf
+++ aws-ec2-terraform1.0/instance.tf
@@ -7,6 +7,15 @@ provider "aws" {
 resource "aws_instance" "web3" {
   ami           = "ami-077d5d3682940b34a"
   instance_type = "t2.micro"
+  tags = {
+  Name = "devops"
 }
+}
+resource "aws_key_pair" "deploy" {
+  key_name = "deploy-key"
+  public_key = "my public-key"
+}
+
+
 
 
--- aws-ec2-terraform1.0/variables.tf
+++ aws-ec2-terraform1.0/variables.tf
@@ -1,6 +1,5 @@
 variable "region" {
   description = "name of the my region at AWS"
-  type = string
   default = "sa-east-1"
 }
 
