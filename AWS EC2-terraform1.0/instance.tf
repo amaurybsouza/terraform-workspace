24829e27b911e6982c7baf4420712c088968a548
amaurybsouza <amaurybsouza@gmail.com>
2020-04-28 12:52:27 -0300

    new file variables.tf

--- AWS EC2-terraform1.0/instance.tf
+++ AWS EC2-terraform1.0/instance.tf
@@ -1,11 +1,11 @@
 provider "aws" {
   version                 = "~> 2.59"
-  region                  = "sa-east-1"
+  region                  = var.region
   shared_credentials_file = "/home/amaury/.aws/credentials"
   profile                 = "terraform-aws-project1"
 }
 
-resource "aws_instance" "web2" {
+resource "aws_instance" "web3" {
   ami           = "ami-077d5d3682940b34a"
   instance_type = "t2.micro"
 }
---
+++ AWS EC2-terraform1.0/variables.tf
@@ -0,0 +1,6 @@
+variable "region" {
+  description = "name of the my region at AWS"
+  type = string
+  default = "sa-east-1"
+}
+
